3c3c597163aa2088468ebf96475d334f295e6198
Chima-Anason <tblynxx@gmail.com>
2021-12-06 02:51:02 +0800

    Initial commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Anason Chima on 5/10/20.
+//  Copyright © 2020 Anason Chima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
